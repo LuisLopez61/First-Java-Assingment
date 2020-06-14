6a4be229772e49aba5a923ec4cc6b0193e6ac350
LuisLopez61 <66345299+LuisLopez61@users.noreply.github.com>
2020-06-14 14:29:06 -0600

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,17 @@
+public class Ex2 {
+    public static void main(String args[]){
+
+    String Name= "Luis";
+    int num1=22;
+    int num2=40000;
+    
+
+    System.out.println(Name);
+    System.out.println(num1);
+    System.out.println(num2);
+    
+   
+   
+    }
+
+}
